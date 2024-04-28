8dceac08e22a948cde22be889156fb23ec5c1496
fersantisi <fer.santisi@hotmail.com>
2024-04-28 17:46:45 -0300

    extract description from html

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 const path = require("path")
-const { log } = require("console")
+const axios = require("axios")
+const cheerio = require("cheerio")
 
 const dataPath = path.join(__dirname, "./data.json")
 let data = JSON.parse(fs.readFileSync(dataPath, "utf-8"))
@@ -11,17 +12,27 @@ const controller = {
         return res.render("index", {phrase: "Hello World"})
     },
 
-    updateCloud: (req, res) => {
+    updateCloud: async (req, res) => {
         let url = req.query.productUrl
+        let id
         console.log(url)
-        let prodDescription = "hello world"
+        const response = await axios.get(url)
+        const $ = cheerio.load(response.data)
+        const prodDescription = await $("#product-summary").children("p").children("span").text().trim()
+        console.log(prodDescription, ": description")
+        if(data[0] == undefined){
+            id = 1
+        }else{
+            id = data[data.length - 1].id + 1
+        }
         let newUrl = {
+            "id": id,
             "url": url,
             "prod": prodDescription
         }
         data.push(newUrl)
         fs.writeFileSync(dataPath, JSON.stringify(data, null, "\t"))
-        return res.render("index", {phrase: url})
+        return res.send(`URL stored (ID:${id})`)
     }
 
 }
