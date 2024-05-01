700e2861fcf6b69e909dcc6fbbdd19d2ae45c0ed
fersantisi <fer.santisi@hotmail.com>
2024-04-30 22:22:33 -0300

    added urlsDequeued.json

--- src/controllers/cloudController.js
+++ src/controllers/cloudController.js
@@ -3,13 +3,17 @@ const path = require("path")
 const cheerio = require("cheerio")
 
 const urlQueuePath = path.join(__dirname, "../data/urlQueue.json")
-//const urlQueue = JSON.parse(fs.readFileSync(urlQueuePath, "utf-8"))
+
+const urlsDequeuedPath = path.join(__dirname, "../data/urlsDequeued.json")
+const urlsDequeued = JSON.parse(fs.readFileSync(urlsDequeuedPath, "utf-8"))
 
 const wordDictionaryPath = path.join(__dirname, "../data/wordDictionary.json")
 const wordDictionary = JSON.parse(fs.readFileSync(wordDictionaryPath, "utf-8"))
 
 const stopWordsPath = path.join(__dirname, "../data/stopWords.txt")
 const stopWords = fs.readFileSync(stopWordsPath, "utf-8")
+
+let urlsDequeuedMap = new Map(Object.entries(urlsDequeued))
 
 let wordMap = new Map(Object.entries(wordDictionary))
 
@@ -27,6 +31,12 @@ const controller = {
         let urlQueueMap = new Map(Object.entries(urlQueue))
 
         for (let [key, value] of urlQueueMap) {
+            if(!urlsDequeuedMap.has(key)){
+                urlsDequeuedMap.set(key, value)
+            }
+        }
+
+        for (let [key, value] of urlsDequeuedMap) {
             if (value == true) {
                 let url = key
                 console.log(url);
@@ -38,7 +48,7 @@ const controller = {
                 });
 
                 const $ = await cheerio.load(await response.text())
-                let prodDescription = await $("#productDescription").children("p").children("span").text().trim()
+                let prodDescription = await $("#productDescription").text().trim()
                 console.log(prodDescription);
                 prodDescription = await prodDescription.replace(/[^a-zA-Z ]/g, "").replace(/\s+/g, " ")
                 const wordArr = await prodDescription.toLowerCase().split(" ")
@@ -55,15 +65,15 @@ const controller = {
                     }
                 })
             }
-            urlQueueMap.set(key, false)
+            urlsDequeuedMap.set(key, false)
         }
 
         const maxVal = Math.max(...wordMap.values())
 
         const wordObj = await Object.fromEntries(wordMap)
-        const urlQueueObj = await Object.fromEntries(urlQueueMap)
+        const urlsDequeuedObj = await Object.fromEntries(urlsDequeuedMap)
 
-        fs.writeFileSync(urlQueuePath, JSON.stringify(urlQueueObj, null, "\t"))
+        fs.writeFileSync(urlsDequeuedPath, JSON.stringify(urlsDequeuedObj, null, "\t"))
         fs.writeFileSync(wordDictionaryPath, JSON.stringify(wordObj, null, "\t"))
 
         await res.render('./index', {map: wordMap, maxVal})
